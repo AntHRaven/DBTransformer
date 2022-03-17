73f7f3baf0ae1d2e296d3dd7cc09b16fcdbd95cd
Nikita Veko <n.veko@iac.spb.ru>
2022-03-17 22:30:05 +0300

    new version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
-import databases.dataBases.PostgresSQLDB;
-import dto.ConnectionData;
-import dto.ConnectionDataMongoImpl;
-import dto.ConnectionDataPostgresImpl;
+import java.sql.Connection;
 import readers.PostgresDBReader;
-
-import java.sql.SQLException;
+import transformers.TransformerToMongoDB;
+import transformers.interfaces.TransformerToDocumentaryDB;
 
 public class Main {
-    public static void main(ConnectionData<PostgresSQLDB> connection) throws SQLException {
-        //DbManager dbManager = new DBManagerPostgresImpl();
-        //dbManager.transform(connection);
-    
-        PostgresDBReader reader = DBReaderManager.getReader(new ConnectionDataPostgresImpl());
-    
-        ConnectionDataMongoImpl connectionDataMongo = new ConnectionDataMongoImpl();
-    }
+  public static void main(String[] args) {
+
+
+    // imagine, user has postgreSQL and want MongoDB
+
+    // connection to user's db (postgre)
+    Connection c = null;
+
+    // initialize transformator for current db (mongo)
+    // call method (from relational to doc) and give it as arg -- postgre reader
+    TransformerToDocumentaryDB transformer = new TransformerToMongoDB();
+    transformer.fromRelationalToDocumentary(new PostgresDBReader(c));
+    System.out.println("");
+    // think over what will we return to user ? new connection to mongo ?
+  }
 }
