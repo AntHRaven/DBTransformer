bc3a03af70e687083226874c57dc4a05b37516ef
Nikita Veko <n.veko@iac.spb.ru>
2022-03-20 16:12:49 +0300

    getAllTablesNames, getAllFieldsNames

--- src/main/java/transformers/TransformerToMongoDB.java
+++ src/main/java/transformers/TransformerToMongoDB.java
@@ -6,28 +6,32 @@ import readers.interfaces.RelationalDBReader;
 import transformers.interfaces.TransformerToDocumentaryDB;
 
 public class TransformerToMongoDB implements TransformerToDocumentaryDB {
-    @Override
-    public void fromRelationalToDocumentary(RelationalDBReader reader) {
-        // when we, for example as in main class, have postgre -> mongo
-        // here we use reader methods
-        // if it's postgre -- PostgresDBReader
 
-        reader.getAllTablesNames();
-        reader.getAllFieldsNames("TableName");
+  @Override
+  public void fromRelationalToDocumentary(RelationalDBReader reader) {
+    // when we, for example as in main class, have postgre -> mongo
+    // here we use reader methods
+    // if it's postgre -- PostgresDBReader
 
-        // using these methods we can create new mongo db from any relational
-        // target of this method = make mongo, using reader's methods (getting data)
-
-        // we need to think about returning value (maybe connection to new mongo db)
+    System.out.println(reader.getAllTablesNames());
+    for (String tableName : reader.getAllTablesNames()) {
+      System.out.println(reader.getAllFieldsNames(tableName));
     }
+//        reader.getAllFieldsNames("TableName");
 
-    @Override
-    public void fromKeyValueToDocumentary(KeyValueDBReader reader) {
+    // using these methods we can create new mongo db from any relational
+    // target of this method = make mongo, using reader's methods (getting data)
 
-    }
+    // we need to think about returning value (maybe connection to new mongo db)
+  }
 
-    @Override
-    public void fromDocumentaryToDocumentary(DocumentaryDBReader reader) {
+  @Override
+  public void fromKeyValueToDocumentary(KeyValueDBReader reader) {
 
-    }
+  }
+
+  @Override
+  public void fromDocumentaryToDocumentary(DocumentaryDBReader reader) {
+
+  }
 }
