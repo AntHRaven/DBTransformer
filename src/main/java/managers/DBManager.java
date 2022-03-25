ec4fe6dc6344abc49efef315c8fceaeaafed1509
Ekaterina <ekaterina.2001@live.com>
2022-03-26 01:41:38 +0300

    Катя всё починила

--- src/main/java/managers/DBManager.java
+++ src/main/java/managers/DBManager.java
@@ -1,24 +1,17 @@
+package managers;
+
+import connection.ConnectionData;
 import readers.interfaces.DBReader;
 import transformers.DBTransformer;
+
 import java.sql.SQLException;
 import java.util.List;
 
-public class DBManager implements IDBManager {
+public interface DBManager {
+    void merge(List<ConnectionData<? extends DBReader, ? extends DBTransformer>> connectionDataList)
+            throws SQLException;
+    void transform(ConnectionData<? extends DBReader, ? extends DBTransformer> from,
+                   ConnectionData<? extends DBReader, ? extends DBTransformer> to)
+            throws SQLException;
 
-    @Override
-    public void merge(List<ConnectionData<? extends DBReader, ? extends DBTransformer>> connectionDataList)
-            throws SQLException {
-
-    }
-    
-    @Override
-    public void transform(ConnectionData<? extends DBReader, ? extends DBTransformer> from,
-                          ConnectionData<? extends DBReader, ? extends DBTransformer> to)
-            throws SQLException {
-    
-        DBReader reader = from.getReader();
-        DBTransformer transformer = to.getTransformer();
-
-        transformer.transform(reader);
-    }
 }
