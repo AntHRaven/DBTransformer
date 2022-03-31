65b6f57fdde6f9f2ca8c21809df9420358d8e2e0
Nikita Veko <n.veko@iac.spb.ru>
2022-04-01 00:40:09 +0300

    refactor transformer

--- src/main/java/merger/DBMerger.java
+++ src/main/java/merger/DBMerger.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface DBMerger {
+    //TODO ошибка при перегрузке метода с листом DataBaseDto
     
     Connection mergeFromReader(List<DataBase<? extends DBReader, ? extends DBTransformer>> dataBaseList);
     
--- src/main/java/transformers/DBTransformer.java
+++ src/main/java/transformers/DBTransformer.java
@@ -8,8 +8,6 @@ import readers.DBReader;
 
 public interface DBTransformer {
 
-    //TODO ошибка при перегрузке метода с листом DataBaseDto
-    
     void transform(DBReader reader, Connection fromConnection, Connection toConnection) throws SQLException;
     
     void transform(DataBaseDto dataBase, Connection fromConnection, Connection toConnection) throws SQLException;
