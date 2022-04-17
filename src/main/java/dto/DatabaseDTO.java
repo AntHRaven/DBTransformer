fa0c896688de979a0937a0d66bd72f78140f5b5b
Ekaterina <ekaterina.2001@live.com>
2022-04-17 20:26:36 +0300

    Providers and new classes in MongoDB + some changes in converters (AAAAAA we dont know what to do !!!! )

--- src/main/java/dto/DatabaseDTO.java
+++ src/main/java/dto/DatabaseDTO.java
@@ -1,18 +1,12 @@
 package dto;
 
 import java.util.Set;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DatabaseDTO implements Cloneable{
   
   Set<TableDTO> tables;
-  String url;
   
   public DatabaseDTO(Set<TableDTO> tables){
     this.tables = tables;
