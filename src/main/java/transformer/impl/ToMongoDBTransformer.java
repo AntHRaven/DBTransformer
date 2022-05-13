cbff142e6204e062e0bd40e7f280495c59d55b9c
Ekaterina <ekaterina.2001@live.com>
2022-05-14 00:07:31 +0300

    Added method for making sub object from relTable

--- src/main/java/transformer/impl/ToMongoDBTransformer.java
+++ src/main/java/transformer/impl/ToMongoDBTransformer.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import converter.ToMongoDBTypeConverter;
-import converter.types.FieldDTOMongoDBTypes;
 import data.TableData;
 import database.Database;
 import database.MongoDB;
@@ -54,31 +56,46 @@ public class ToMongoDBTransformer implements DBTransformer {
         Statement statementFrom = connectionFrom.createStatement();
         String selectQuery = "SELECT " + getListOfOldFieldsNames(fields) + " FROM " + tableData.getOldName();
         ResultSet rows = statementFrom.executeQuery(selectQuery);
-    
-        // TODO: 11.05.2022 if FK - do sub object
         
         while (rows.next()) {
-            List<String> fieldsValues = new ArrayList<>();
-            for (String oldFieldName : fields.keySet()) {
-                fieldsValues.add(rows.getString(oldFieldName));
-            }
             Document document = new Document();
-            Map<String, Object> values = new HashMap<>();
-    
-            for (String key : fields.keySet()) {
-                values.put(fields.get(key).getName(), getValue(fields.get(key), fieldsValues.get(0)));
-                fieldsValues.remove(0);
-            }
-            
-            document.putAll(values);
+            document.putAll(getValues(fields, rows, connectionFrom));
             documents.add(document);
         }
         collection.insertMany(documents);
     }
- 
-    private Object getValue(FieldDTO field, String value){
-        Class<?> marker = ((FieldDTOMongoDBTypes) field.getType()).getTypeClass();
-        return marker.cast(value);
+    
+    private DBObject makeSubObject(FieldDTO field, String value, Connection connection, Map<String, FieldDTO> fields) throws SQLException {
+        
+        String oldRelTableName = field.getFK().getRelTableName();
+        String oldRelFieldName = field.getFK().getRelFieldName();
+        
+        Statement statementFrom = connection.createStatement();
+        String selectQuery = "SELECT * FROM " + oldRelTableName + " WHERE " + oldRelFieldName + "=" + value;
+        ResultSet row = statementFrom.executeQuery(selectQuery);
+        
+        return new BasicDBObject(getValues(fields, row, connection));
+    }
+    
+    private Map<String, Object> getValues(Map<String, FieldDTO> fields, ResultSet res, Connection connection) throws SQLException {
+       
+            List<String> fieldsValues = new ArrayList<>();
+            for (String oldFieldName : fields.keySet()) {
+                fieldsValues.add(res.getString(oldFieldName));
+            }
+            
+            Map<String, Object> values = new HashMap<>();
+        
+            for (String key : fields.keySet()) {
+                FieldDTO field = fields.get(key);
+                if (field.getFK() == null) {
+                    values.put(field.getName(), fieldsValues.get(0));
+                } else {
+                    values.put(field.getName(), makeSubObject(field, fieldsValues.get(0), connection, fields));
+                }
+                fieldsValues.remove(0);
+            }
+            return values;
     }
     
     private void createAllDocumentsAndFillData() {
