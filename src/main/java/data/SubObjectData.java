26c596da2728521d7d01e3f0cc0f56633b11610b
Ekaterina <ekaterina.2001@live.com>
2022-05-20 21:12:07 +0300

    fixed constructor

--- src/main/java/data/SubObjectData.java
+++ src/main/java/data/SubObjectData.java
@@ -1,6 +1,7 @@
 package data;
 
 import dto.FieldDTO;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
@@ -8,7 +9,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bson.Document;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class SubObjectData {
@@ -18,5 +18,13 @@ public class SubObjectData {
   private Document field;
   private String newTableName;
   private Map<String, FieldDTO> fields;
+
+  public SubObjectData(Map<String, String> values, Map<NameType, List<String>> mapName, Document field, String newTableName, Map<String, FieldDTO> fields){
+    this.values = new HashMap<>(values);
+    this.mapName = mapName;
+    this.field = field;
+    this.fields = fields;
+    this.newTableName = newTableName;
+  }
 
 }
